e604afbe1603a55a235298c69851f791973a847c
eduardo <eduardodeassis@gmail.com>
2024-07-24 11:39:49 -0300

    Alteração para tocar um beep ao finalizar a contagem.

--- src/Temporizador.java
+++ src/Temporizador.java
@@ -1,9 +1,54 @@
 import javax.swing.JTextField;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
 
 public class Temporizador {
 	int Minuto = 0;
 	int Segundo = 0;
 	int Iniciado = 0;
+	private File arqBeep = new File("src/som/warning-sound-6686.wav");
+	
+    private void beep() {
+    	AudioInputStream din = null;
+        try {
+            AudioInputStream in = AudioSystem.getAudioInputStream(arqBeep.getAbsoluteFile());
+            AudioFormat baseFormat = in.getFormat();
+            AudioFormat decodedFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED,
+                    baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+                    baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
+                    false);
+            din = AudioSystem.getAudioInputStream(decodedFormat, in);
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, decodedFormat);
+            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+            if(line != null) {
+                line.open(decodedFormat);
+                byte[] data = new byte[4096];
+                // Start
+                line.start();
+
+                int nBytesRead;
+                while ((nBytesRead = din.read(data, 0, data.length)) != -1) {
+                    line.write(data, 0, nBytesRead);
+                }
+                // Stop
+                line.drain();
+                line.stop();
+                line.close();
+                din.close();
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(din != null) {
+                try { din.close(); } catch(IOException e) { }
+            }
+        }
+    }
 	 
 	public void setTemporizador(int iMinuto, int iSegundo) {
 		Minuto = iMinuto;
@@ -62,6 +107,7 @@ public class Temporizador {
 					decrementa(0, 1);
 					if (Minuto + Segundo == 0) {
 						Iniciado = 0;
+						beep();
 					}
 					System.out.println(txtFormatado());
 					txt.setText(txtFormatado());
